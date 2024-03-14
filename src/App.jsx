681199433adfc93e4ee44f8a0119277fc9a32082
Kenneth <kinetzki2k20@gmail.com>
2024-03-14 09:55:35 +0800

    feat: Folder Structure and default components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
+import {Routes, Route} from "react-router-dom";
+import LogIn from './pages/LogIn';
+import SignUp from './pages/SignUp';
 
 function App() {
 
   return (
     <>
-      
+      <Routes>
+        <Route exact path="/" element={<LogIn/>}/>
+        <Route exact path="/signup" element={<SignUp/>}/>
+      </Routes>
     </>
   )
 }
